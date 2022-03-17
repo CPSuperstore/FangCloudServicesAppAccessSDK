d75eb3892c3e979302db75174eed31926fed5ab3
Christopher <cpsuperstoreinc@gmail.com>
2022-03-17 00:04:10 -0400

    Fixed models package not being included in build

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+packages = find_packages(where=".")
+
+if "tests" in packages:
+    packages.remove("tests")
+
 setup(
     name='FCSAppAccessSDK',
     version='1.0',
-    packages=['FCSAppAccess'],
+    packages=packages,
     url='https://github.com/CPSuperstore/FangCloudServicesAppAccessSDK',
     license='Apache License 2.0',
     author='CPSuperstore',
